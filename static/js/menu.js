f40e859e5a8649191f5e516ddf16e75151fdbf25
vitalii_denysiuk <dendoc97@gmail.com>
2017-04-09 17:23:34 +0300

    menu for Safari

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,7 +1,15 @@
+(function () {
   var header = document.getElementsByTagName('header')[0];
   var iconMenu = document.getElementById('icon-menu');
   var menuList = document.getElementById('menu-list');
+  var menuLink = document.querySelectorAll('menu-link');
+  var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 
+  if (isSafari) {
+      for (var i = 0; i < menuLink.length; i++) {
+        if (menuLink[i].innerHTML == 'Work') menuLink[i].parentElement.style.display = 'none';
+      }
+  }
   menuList.style.top = header.offsetHeight + 30 + 'px';
   iconMenu.addEventListener('click', function () {
     menuList.classList.toggle('show-menu');
@@ -12,3 +20,5 @@
     scrolled = window.pageYOffset || document.documentElement.scrollTop;
     (scrolled > 100) ? header.classList.add('grey') : header.classList.remove('grey');
   });
+
+})();
