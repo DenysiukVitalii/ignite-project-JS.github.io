99199b279bcc07353d9be1c4b950e133eef2ca00
vitalii_denysiuk <dendoc97@gmail.com>
2017-04-09 17:33:54 +0300

    fix bug with Safari

--- static/js/menu.js
+++ static/js/menu.js
@@ -2,15 +2,16 @@
   var header = document.getElementsByTagName('header')[0];
   var iconMenu = document.getElementById('icon-menu');
   var menuList = document.getElementById('menu-list');
-  var menuLink = document.querySelectorAll('menu-link');
+  var menuLink = document.querySelectorAll('.menu-link');
   var isSafari = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/);
 
   if (isSafari) {
       for (var i = 0; i < menuLink.length; i++) {
-        if (menuLink[i].innerHTML == 'Work') menuLink[i].parentElement.style.display = 'none';
+        if (menuLink[i].dataset.scrollTo == '#portfolio') menuLink[i].parentElement.style.display = 'none';
       }
   }
-  menuList.style.top = header.offsetHeight + 30 + 'px';
+
+  menuList.style.top = header.offsetHeight + 'px';
   iconMenu.addEventListener('click', function () {
     menuList.classList.toggle('show-menu');
   });
